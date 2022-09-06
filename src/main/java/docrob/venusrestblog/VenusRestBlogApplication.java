07b0cfa03e34e5e746c8b16375d16c0077e74621
Docrob <mark.robinson@codeup.com>
2022-09-06 15:03:47 -0500

    first commit

---
+++ src/main/java/docrob/venusrestblog/VenusRestBlogApplication.java
@@ -0,0 +1,13 @@
+package docrob.venusrestblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenusRestBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VenusRestBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/docrob/venusrestblog/VenusRestBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package docrob.venusrestblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenusRestBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
