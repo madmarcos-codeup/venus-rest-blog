4283283361d77570bf253374e4f29e2408c396e8
Docrob <mark.robinson@codeup.com>
2022-09-07 15:07:07 -0500

    request mapping example

--- src/main/java/docrob/venusrestblog/VenusRestBlogApplication.java
+++ src/main/java/docrob/venusrestblog/VenusRestBlogApplication.java
@@ -5,7 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-//@ComponentScan(basePackages = "docrob.venusrestblog.controller")
+/*
+Since Spring Boot 1.2.0, we can use the @SpringBootApplication annotation, which is a combination of the three annotations @Configuration, @EnableAutoConfiguration, and@ComponentScan with their default attributes.
+so... we don't seem to need ComponentScan
+may still need something for repositories
+ */
+//@ComponentScan(basePackages = "docrob.venusrestblog")
 public class VenusRestBlogApplication {
 
     public static void main(String[] args) {
