f8b5d69f519873dcb4a156932b551d619e359eaa
Docrob <mark.robinson@codeup.com>
2022-10-06 12:38:27 -0500

    fixed doLogin identifier

--- src/main/java/docrob/venusrestblog/controller/ViewController.java
+++ src/main/java/docrob/venusrestblog/controller/ViewController.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class ViewController {
-    @GetMapping(value="dologin")
+    @GetMapping(value="doLogin")
     public RedirectView oauthRedirect(RedirectAttributes attributes) {
     //http://localhost:8081/dologin#state=pass-through%20value&access_token=ya29.a0Aa4xrXPfpUa2mRv4JXSP6f8qHeVeA4fJN41O77KarS6SpLzLUstb5sKm4cJXuWBmY4eVDmqqwvX2xybM6NGan_mYHkv3cF8llpZpDnlXEJrW6k29fEWXvNbvZmWAYJd84bn-OvHblwoyU79YcApK_7dCjfjNhAaCgYKATASAQ4SFQEjDvL9ZmxxCFXg3pfZxnllIPqp8w0165&token_type=Bearer&expires_in=3599&scope=email%20profile%20openid%20https://www.googleapis.com/auth/classroom.rosters.readonly%20https://www.googleapis.com/auth/userinfo.profile%20https://www.googleapis.com/auth/classroom.student-submissions.students.readonly%20https://www.googleapis.com/auth/classroom.courses.readonly%20https://www.googleapis.com/auth/drive.metadata.readonly%20https://www.googleapis.com/auth/userinfo.email&authuser=1&hd=codeup.com&prompt=none
         System.out.println("oauthRedirect called");
--- src/main/resources/static/js/private_constants.js
+++ src/main/resources/static/js/private_constants.js
@@ -1,3 +1,3 @@
-const CLIENT_ID = "306956147499-5f1jmg119d7sv9opug8napfmcf3mdnft.apps.googleusercontent.com";
-const REDIRECT_URL = "http://localhost:8081/dologin";
+const CLIENT_ID = "807309833487-uvihhc5be5ie00qj13a9d20kt9sovlp6.apps.googleusercontent.com";
+const REDIRECT_URL = "http://localhost:8081/doLogin";
 const SCOPES = "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile";
