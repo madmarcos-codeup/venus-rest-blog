2e277e36a5826687ba952ec938a8e23e3f494160
Docrob <mark.robinson@codeup.com>
2022-10-07 14:16:27 -0500

    cleanup

--- src/main/resources/static/js/init.js
+++ src/main/resources/static/js/init.js
@@ -1,14 +1,7 @@
 import createView from './createView.js';
-import {checkForLoginTokens, setLoggedInUserInfo} from './auth.js';
 
 
 export default function init() {
-    // NOTE: the below is not needed for Google Identity
-    // if(checkForLoginTokens(window.location.href)) {
-    //     setLoggedInUserInfo();
-    //     createView("/");
-    //     return;
-    // }
     loadViewOnPageRequest();
     addListenerToNavLinks();
 }
